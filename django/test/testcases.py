edf4db16b0cab74f65721ee11630796b46185748
Chris Jerdonek <chris.jerdonek@gmail.com>
2016-08-19 09:47:41 -0700

    Fixed #22414 -- Ensured that LiveServerTestCase closes connections.
    Cherry-picked from upstream/stable/1.11.x:
    
      f6cd669ff203192c29495174e53da6b16883b039
    
    Instead of merging from shotwell/patches/backports-1.11-to-1.9-root:
    
      dcc5b0aa29777865aa164b6ad1ab0d4a7d828f59

--- django/test/testcases.py
+++ django/test/testcases.py
@@ -1274,6 +1274,8 @@ class LiveServerThread(threading.Thread):
         except Exception as e:
             self.error = e
             self.is_ready.set()
+        finally:
+            connections.close_all()
 
     def _create_server(self, port):
         return WSGIServer((self.host, port), QuietWSGIRequestHandler, allow_reuse_address=False)
@@ -1283,6 +1285,7 @@ class LiveServerThread(threading.Thread):
             # Stop the WSGI server
             self.httpd.shutdown()
             self.httpd.server_close()
+        self.join()
 
 
 class LiveServerTestCase(TransactionTestCase):
@@ -1369,7 +1372,6 @@ class LiveServerTestCase(TransactionTestCase):
         if hasattr(cls, 'server_thread'):
             # Terminate the live server's thread
             cls.server_thread.terminate()
-            cls.server_thread.join()
 
         # Restore sqlite in-memory database connections' non-shareability
         for conn in connections.all():
